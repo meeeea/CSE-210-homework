1dd8391344b683c283894480b3a3e3b9d4ede99f
Justin Hutchens <Justinh22007@gmail.com>
2024-02-26 09:05:08 -0600

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> the_list = new List<int>();
+        while (true) {
+            int input;
+            try {
+                Console.WriteLine("Enter Number: ");
+                input = Int32.Parse(Console.ReadLine());
+            }
+            catch {
+                continue;
+            }
+            if (input == 0 && the_list.Count > 1) {
+                break;
+            }
+
+            the_list.Add(input);
+        }
+        int largest = 0;
+        int total = 0;
+        foreach (int i in the_list) {
+            if (largest < i) {
+                largest = i;
+            }
+            total += i;
+        }
+        float average = (float)total / (float)the_list.Count;
+
+        Console.WriteLine($"The largest number listed was {largest}");
+        Console.WriteLine($"The total of the numbers listed was {total}");
+        Console.WriteLine($"The average of the numbers listed was {average}");
+
     }
 }
