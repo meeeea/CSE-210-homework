dfb23160c089b567d0d0dd5f6e4c830e6d4cf6ad
Justin Hutchens <Justinh22007@gmail.com>
2024-02-26 08:46:59 -0600

    i did prep 2 and prep 3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int result;
+        while (true) {
+            try {
+                Console.WriteLine("Grade: ");
+                result = Int32.Parse(Console.ReadLine());
+            }
+            catch {
+                continue;
+            }
+            break;
+        }
+        bool passes = false;
+        string grade = "f";
+
+        if (result >= 90) {
+            grade = "a";
+            passes = true;
+        }
+        else if (result >= 80) {
+            grade = "b";
+            passes = true;
+        }
+        else if (result >= 70) {
+            grade = "c";
+            passes = true;
+        }
+        else if (result >= 60) {
+            grade = "d";
+        }
+
+        if (passes) {
+            Console.WriteLine($"Congradulations, you passed with a(n) {grade}");
+        }
+        else {
+            Console.WriteLine($"Saddly, you failed with a(n) {grade}");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magic_number = new Random().Next(100);
+        
+        int guesses = 0;
+        while (true) {
+            int guess;
+
+            try {
+                Console.WriteLine("Guess: ");
+                guess = Int32.Parse(Console.ReadLine());
+            }
+            catch {
+                continue;
+            }
+            if (guess > magic_number) {
+                Console.WriteLine("Lower");
+                guesses++;
+            }
+            else if (guess < magic_number) {
+                Console.WriteLine("Higher");
+                guesses++;
+            }
+            else {
+                Console.WriteLine($"Congratulations, the correct number was {magic_number}");
+                Console.WriteLine($"You guessed the correct number in {guesses} attempts");
+                break;
+            }
+        }
     }
 }
