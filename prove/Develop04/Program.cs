22506474f8ddcf21ea1ff9d09959b8dc6f8d56db
Justin Hutchens <Justinh22007@gmail.com>
2024-03-19 20:29:19 -0500

    added menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        new ListingActivity();
+        while(true) {
+            Console.WriteLine("1). Breathing Activity");
+            Console.WriteLine("2). Reflection Activity");
+            Console.WriteLine("3). Listing Activity");
+            Console.WriteLine("4). Quit");
+
+            int option = Console.Read() - 48;
+
+            if (option == 1) {
+                new BreathingActivity();
+            }
+            else if (option == 2) {
+                new ReflectionActivity();
+            }
+            else if (option == 3) {
+                new ListingActivity();
+            }
+            else {
+                break;
+            }
+        }
     }
 }
