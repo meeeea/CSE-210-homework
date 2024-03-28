42050609571e8c0f745f6a4d0c04c998a18e6939
Justin Hutchens <Justinh22007@gmail.com>
2024-03-27 19:39:33 -0500

    added info for gradders

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,5 +1,8 @@
 using System;
 
+// the checklist goals show what the next step is.
+// there is a level manager that keeps track of leveling up
+
 class Program
 {
     static void Main(string[] args)
