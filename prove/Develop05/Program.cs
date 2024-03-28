97ab216fc3923ebc0ed3d5b390f15e323b5406d5
Justin Hutchens <Justinh22007@gmail.com>
2024-03-27 19:40:01 -0500

    removed unused info

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,11 +1,9 @@
-using System;
-
 // the checklist goals show what the next step is.
 // there is a level manager that keeps track of leveling up
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         GoalManager goalManager = new GoalManager();
         goalManager.MainLoop();
